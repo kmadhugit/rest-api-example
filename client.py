ab70b988455496aef709a1e4cd96062aaa0b87c6
Madhusudanan Kandasamy <madhukandasamy@yahoo.com>
2018-12-11 21:37:23 +0530

    json send

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 # https://stackoverflow.com/questions/4476373/simple-url-get-post-function-in-python
+# http://docs.python-requests.org/en/master/user/quickstart/
 # import requests
 # url = 'https://...'
 # payload = {'key1': 'value1', 'key2': 'value2'}
@@ -12,9 +13,12 @@
 # # POST with form-encoded data
 # r = requests.post(url, data=payload)
 #
+# # POST with form-encoded data and params
+# r = requests.post(url, params=payload1, data=payload)
+
 # # POST with JSON
 # import json
-# r = requests.post(url, data=json.dumps(payload))
+# r = requests.post(url, json=payload)
 #
 # # Response, status etc
 # r.text
@@ -30,15 +34,21 @@ port     = 5000
 URL = f'http://{hostname}:{port}/get'
 
 # defining a params dict for the parameters to be sent to the API
-PARAMS = {'name': 'madhu'}
+payload1 = {'name': 'madhu1'}
+payload2 = {'name': 'madhu2','age':30}
 
-# sending get request and saving the response as response object
-r = requests.get(url=URL, params=PARAMS)
+r = requests.get(url=URL, params=payload1,data=payload2)
 data = r.json()
 print(type(r),type(data),data)
 
-
-r = requests.post(url=URL, params=PARAMS)
+r = requests.post(url=URL, params=payload1,data=payload2)
 data = r.json()
 print(type(r),type(data),data)
 
+r = requests.get(url=URL, params=payload1,json=payload2)
+data = r.json()
+print(type(r),type(data),data)
+
+r = requests.post(url=URL, params=payload1,json=payload2)
+data = r.json()
+print(type(r),type(data),data)
--- server.py
+++ server.py
@@ -2,27 +2,32 @@ from flask import Flask, request, jsonify
 
 # https://stackoverflow.com/questions/10434599/how-to-get-data-received-in-flask-request
 # http://flask.pocoo.org/docs/1.0/quickstart/#about-responses
+# http://docs.python-requests.org/en/master/user/quickstart/
 
 app = Flask(__name__)
 ret = {}
 
+def printit(msg,dict):
+    if dict is not None:
+        for k,v in dict.items():
+            print(msg,k,v)
 
-@app.route('/get', methods=['GET'])
+@app.route('/get', methods=['GET','POST'])
 def doget(): # This is called view function
-    print(request.method)
-    for k,v in request.args.items():
-        print(k,v)
-    name = request.args['name']
-    ret['name'] = 'Received_via_get' + name
-    return jsonify(ret)
 
-@app.route('/get', methods=['POST'])
-def dopost():
     print(request.method)
-    for k,v in request.args.items():
-        print(k,v)
-    name = request.args['name']
-    ret['name'] = 'Received_via_post' + name
+    printit('request.args',request.args)
+    printit('request.form',request.form)
+    printit('request.values',request.values) # values = args union form, if duplicate,args gets precedence
+    printit('request.json',request.json) # values = args union form, if duplicate,args gets precedence
+
+    if request.content_type == 'application/json':
+        k = request.json
+    else:
+        k = request.values
+
+    name = k['name']
+    ret['name'] = f'{request.method} :' + name
     return jsonify(ret)
 
 
